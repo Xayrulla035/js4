c2f8c953fa3f1a4ab371619d9b102aa5056f0f1b
Xayrulla <qoralama624@gmail.com>
2026-01-17 20:30:00 +0500

    js4

---
+++ script.js
@@ -0,0 +1,14 @@
+let name = prompt("Ismingizni kiriting");
+let xozir = +prompt("Xozirgi yilni kiriting");
+let oldin = +prompt("tug'ilgan yilingizni kiriting");
+
+function summ(xozir, oldin) {
+    return xozir - oldin
+}
+alert (name + ", sizning yoshingiz" + summ(xozir, oldin));
+
+
+
+
+
+
